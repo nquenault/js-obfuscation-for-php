b3ed2f9699495d7811d7b3cafcdee8c44c43b6d6
Nicolas Quenault <nquenault@gmail.com>
2017-07-12 17:23:34 +0200

    Update js-obfuscator.php

--- lib/js-obfuscator.php
+++ lib/js-obfuscator.php
@@ -16,86 +16,85 @@ class JSObfuscator
 
 	public static function getFile($filepath)
 	{
+		if(!file_exists($filepath))
+		return null;
+		
 		$cachepath = self::$cacheFolder.md5($filepath).'.'.md5_file($filepath).'.'.basename($filepath); // cache file path
 		$script = null;
 
 		// clear old cache versions of $filepath
 		self::clearCache(md5($filepath).'.*.'.basename($filepath), preg_quote(basename($cachepath), '@').'$');
 
-		if(file_exists($filepath))
+		$script = file_get_contents($filepath);
+		$obf_args = '-f '.$filepath;
+
+		$obfuscate = null;
+		$use_cache = null;
+		$create_cache = null;
+		$update_cache = null;
+
+		if(preg_match('@/[/\*]!jsobf\s+([^\r\n]+)@i', $script, $match))
 		{
-			$script = file_get_contents($filepath);
-			$obf_args = '-f '.$filepath;
+			$match[1] = preg_replace('@(false|off|no)@i', '0', $match[1]);
+			$match[1] = preg_replace('@(true|on|yes)@i', '1', $match[1]);
 
-			$obfuscate = null;
-			$use_cache = null;
-			$create_cache = null;
-			$update_cache = null;
-
-			if(preg_match('@/[/\*]!jsobf\s+([^\r\n]+)@i', $script, $match))
+			if(preg_match('@\-obf[\s=]+([01])@i', $match[1], $match_obf))
 			{
-				$match[1] = preg_replace('@(false|off|no)@i', '0', $match[1]);
-				$match[1] = preg_replace('@(true|on|yes)@i', '1', $match[1]);
-
-				if(preg_match('@\-obf[\s=]+([01])@i', $match[1], $match_obf))
+				switch($match_obf[1])
 				{
-					switch($match_obf[1])
-					{
-						case '0':
-							$obfuscate = false;
-							break;
-						case '1':
-							$obfuscate = true;
-							break;
-					}
-				}
-
-				if(preg_match('@\-cache[\s=]+([01]|clear|(re)?set|update)@i', strtolower($match[1]), $match_cache))
-				{
-					switch($match_cache[1])
-					{
-						case 'reset':
-						case 'update':
-							$update_cache = true;
-							$use_cache = true;
-						case 'clear':
-							if(file_exists($cachepath))
-							unlink($cachepath);
-						case '0':
-							$use_cache = $use_cache != null ? $use_cache : false;
-							break;
-						case 'set':
-						case '1':
-							$create_cache = true;
-							$use_cache = true;
-							break;
-					}
+					case '0':
+						$obfuscate = false;
+						break;
+					case '1':
+						$obfuscate = true;
+						break;
 				}
 			}
 
-			if($obfuscate)
+			if(preg_match('@\-cache[\s=]+([01]|clear|(re)?set|update)@i', strtolower($match[1]), $match_cache))
 			{
-				$obf_script = null;
-
-				if(is_dir(self::$cacheFolder))
-				if(($create_cache && !file_exists($cachepath)) || ($update_cache && file_exists($cachepath)))
+				switch($match_cache[1])
 				{
-					$obf_args .= ' -dci 1'; // enable dead code injection
-					$obf_script = sys::exec('nodejs '.self::$nodeJSObf.' '.$obf_args);
-
-					file_put_contents($cachepath, $obf_script);
+					case 'reset':
+					case 'update':
+						$update_cache = true;
+						$use_cache = true;
+					case 'clear':
+						if(file_exists($cachepath))
+						unlink($cachepath);
+					case '0':
+						$use_cache = $use_cache != null ? $use_cache : false;
+						break;
+					case 'set':
+					case '1':
+						$create_cache = true;
+						$use_cache = true;
+						break;
 				}
+			}
+		}
 
-				if($use_cache && file_exists($cachepath))
-				$script = file_get_contents($cachepath);
-				else
-				{
-					if(!$obf_script)
-					$obf_script = sys::exec('nodejs '.self::$nodeJSObf.' '.$obf_args);
+		if($obfuscate)
+		{
+			$obf_script = null;
 
-					$script = $obf_script;
-				}
+			if(is_dir(self::$cacheFolder))
+			if(($create_cache && !file_exists($cachepath)) || ($update_cache && file_exists($cachepath)))
+			{
+				$obf_args .= ' -dci 1'; // enable dead code injection
+				$obf_script = sys::exec('nodejs '.self::$nodeJSObf.' '.$obf_args);
 
+				file_put_contents($cachepath, $obf_script);
+			}
+
+			if($use_cache && file_exists($cachepath))
+			$script = file_get_contents($cachepath);
+			else
+			{
+				if(!$obf_script)
+				$obf_script = sys::exec('nodejs '.self::$nodeJSObf.' '.$obf_args);
+
+				$script = $obf_script;
 			}
 		}
 
